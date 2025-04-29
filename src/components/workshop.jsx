edebac59d6ec508c4eeb2a65271cf37a45e7d167
devan <a.d.devnandan@gmail.com>
2025-04-30 01:33:51 +0530

    beam vannu guys

--- src/components/workshop.jsx
+++ src/components/workshop.jsx
@@ -11,11 +11,12 @@ import Image from 'next/image';
 import { GlareCard } from "@/components/ui/glare-card";
 import { GradualSpacing } from "@/components/ui/gradual-spacing";
 import { getAllEvents, domain } from '@/data/data-placeholder';
+import {BackgroundBeams} from "@/components/backgroundbeem";
 
 const SwiperCoverflow = () => {
     const router = useRouter();
     const events = getAllEvents();
-    const [backgroundImage, setBackgroundImage] = useState('/images/galaxy-space-textured-background.jpg');
+    const [backgroundImage, setBackgroundImage] = useState('');
     const [activeIndex, setActiveIndex] = useState(0);
     const swiperRef = useRef(null);
 
@@ -54,13 +55,13 @@ const SwiperCoverflow = () => {
             <div className="absolute top-32 left-1/2 -translate-x-1/2 text-center z-20 space-y-10">
                 <GradualSpacing text="Flagship Events" />
                 <button
-                    className="bg-slate-800 no-underline mt-10 group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6 text-white inline-block"
+                    className="bg-slate-800 no-underline mt-10 mb-10 group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6 text-white inline-block"
                     onClick={() => router.push(domain)}
                 >
                     <span className="absolute inset-0 overflow-hidden rounded-full">
                         <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
                     </span>
-                    <div className="relative flex space-x-2 items-center rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10">
+                    <div className="relative flex  space-x-2 items-center rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10">
                         <span>View More</span>
                         <svg
                             width="16"
@@ -160,6 +161,9 @@ const SwiperCoverflow = () => {
                     </motion.div>
                 </div>
             </div>
+            {!backgroundImage && (
+                <BackgroundBeams/>
+            )}
         </div>
     );
 };
