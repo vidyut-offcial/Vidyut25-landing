58de61ad5ddbf0cfbe6e90fbb0267cc6e35c9f0d
devan <a.d.devnandan@gmail.com>
2025-04-30 02:28:12 +0530

    proshow responsive aki!!!

--- src/components/ThreeDMarquee.jsx
+++ src/components/ThreeDMarquee.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { motion } from "motion/react";
-import {cn} from "@/lib/utils";
+import { cn } from "@/lib/utils";
 
 export const ThreeDMarquee = ({ images, className }) => {
     const chunkSize = Math.ceil(images.length / 6);
@@ -12,15 +12,22 @@ export const ThreeDMarquee = ({ images, className }) => {
     });
 
     return (
-        <div className={cn("mx-auto block h-[1000px]  overflow-hidden rounded-2xl max-sm:h-100", className)}>
+        <div className={cn("mx-auto block overflow-hidden rounded-2xl h-[700px] sm:h-[1000px]", className)}>
             <div className="flex size-full items-center justify-center">
 
-                <div className="size-[2320px] shrink-0 scale-100 sm:scale-75 lg:scale-100">
+                <div className="w-full sm:w-[1320px] md:w-[1920px] lg:w-[2320px] shrink-0 transform scale-95 sm:scale-75 lg:scale-100 mx-auto">
                     <div
-                        style={{
-                            transform: "rotateX(35deg) rotateY(-19deg) rotateZ(20deg)",
-                        }}
-                        className="relative top-1/4 left-1/8 grid size-full grid-cols-6 gap-12 transform-3d"
+                        className={`
+                            relative 
+                            grid 
+                            grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 
+                            gap-4 sm:gap-8 lg:gap-12 
+                            items-center justify-center 
+                            w-full h-full
+                            [transform-style:preserve-3d]
+                            transform
+                            sm:[transform:rotateX(35deg)_rotateY(-19deg)_rotateZ(20deg)]
+                        `}
                     >
                         {chunks.map((subarray, colIndex) => (
                             <motion.div
@@ -31,7 +38,7 @@ export const ThreeDMarquee = ({ images, className }) => {
                                     repeatType: "reverse",
                                 }}
                                 key={`${colIndex}-marquee`}
-                                className="flex flex-col items-start gap-8"
+                                className="flex flex-col items-center gap-6"
                             >
                                 {subarray.map((image, imageIndex) => (
                                     <div className="relative" key={`${imageIndex}-${image}`}>
