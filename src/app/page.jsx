4e9a5a825fe8521d7a1501a459a695b1b1470276
devan <a.d.devnandan@gmail.com>
2025-04-30 14:45:55 +0530

    spaceship removed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,253 +1,137 @@
 "use client";
 
-import AboutSection from "@/components/AboutSection";
+
 import HeroSection from "@/components/HeroSection";
-import IdeaSection from "@/components/IdeaSection";
 import NavBar from "@/components/NavBar";
-import PostLoading from "@/components/PostLoading";
-import SpaceShipModel from "@/models/SpaceShipModel";
-import gsap from "gsap";
-import { useEffect, useRef, useState } from "react";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { SpaceshipContext } from "@/contexts/SpaceShipContext";
-import ReactHowler from "react-howler";
-import Footer from "@/components/Footer";
+import AboutSection from "@/components/AboutSection";
+import IdeaSection from "@/components/IdeaSection";
 import FAQSection from "@/components/FAQSection";
-import dynamic from 'next/dynamic';
+import Footer from "@/components/Footer";
+import {useEffect, useRef, useState} from "react";
+import dynamic from "next/dynamic";
+import gsap from "gsap";
 
-gsap.registerPlugin(ScrollTrigger);
+const Home = () => {
+    const [showSpaceship, setShowSpaceship] = useState(true);
+    const [isSpaceshipMounted, setIsSpaceshipMounted] = useState(true);
+    const spaceshipRef = useRef(null);
+    const mainRef = useRef(null);
+    const [playOne, setPlayOne] = useState(false);
+    const [playTwo, setPlayTwo] = useState(false);
+    const howlerOneRef = useRef();
+    const howlerTwoRef = useRef();
 
-export default function Home() {
-  const [showSpaceship, setShowSpaceship] = useState(true);
-  const [isSpaceshipMounted, setIsSpaceshipMounted] = useState(true);
-  const spaceshipRef = useRef(null);
-  const mainRef = useRef(null);
-  const [playOne, setPlayOne] = useState(false);
-  const [playTwo, setPlayTwo] = useState(false);
-  const howlerOneRef = useRef();
-  const howlerTwoRef = useRef();
+    const LazyProshow = dynamic(() => import('@/app/proshow/page'), {
+        loading: () => <div className="text-center p-4">Loading Proshow...</div>,
+        ssr: false,
+    });
 
-  const LazyProshow = dynamic(() => import('@/app/proshow/page'), {
-    loading: () => <div className="text-center p-4">Loading Proshow...</div>,
-    ssr: false,
-  });
+    const LazyAutoshowSection = dynamic(() => import('@/components/autoshow/AutoshowSection'), {
+        loading: () => <div className="text-center p-4">Loading Autoshow...</div>,
+        ssr: false,
+    });
 
-  const LazyAutoshowSection = dynamic(() => import('@/components/autoshow/AutoshowSection'), {
-    loading: () => <div className="text-center p-4">Loading Autoshow...</div>,
-    ssr: false,
-  });
+    const LazySwiperCoverflow = dynamic(() => import('@/components/workshop'), {
+        loading: () => <div className="text-center p-4">Loading Workshops...</div>,
+        ssr: false,
+    });
 
-  const LazySwiperCoverflow = dynamic(() => import('@/components/workshop'), {
-    loading: () => <div className="text-center p-4">Loading Workshops...</div>,
-    ssr: false,
-  });
-
-  const startReveal = () => {
-    const tl = gsap.timeline();
-    gsap.set("#hero-section", { opacity: 1 });
-    gsap.set("#hero-title", { y: 100 });
-    gsap.set("#hero-subtitle", { x: -100, opacity: 0, y: 0 });
-    gsap.set("#hero-comingsoon", { y: 30, opacity: 0 });
-    gsap.set("#hero-countdown", { scale: 0.95, opacity: 0 });
-
-    tl
-      .to("#hero-title", {
-        duration: 2,
-        y: 0,
-        ease: "power2.out",
-        onComplete: () => {
-          gsap.set("#model", { translateY: "100vh", position: "absolute", top: 0, left: 0, zIndex: "-10" });
-        },
-      })
-      .to("#hero-title", {
-        duration: 2,
-        y: 0,
-        ease: "power2.inOut",
-      })
-      .to("#hero-subtitle", {
-        x: 0,
-        opacity: 2,
-        duration: 1.2,
-        ease: "power3.out",
-      }, "-=0.5")
-      .set("#section-container", { display: "block" })
-      .to("#hero-comingsoon", {
-        y: 0,
-        opacity: 1,
-        duration: 2,
-        ease: "power2.out",
-        onComplete: () => {
-          gsap.to("#section-container", {
-            opacity: 1,
-            duration: 0.5,
-            ease: "power2.out",
-          });
-        },
-      }, "-=0.8")
-      .to("#hero-countdown", {
-        scale: 1,
-        opacity: 1,
-        duration: 2,
-        ease: "back.out(1.7)",
-      }, "-=0.8");
-  };
-
-  function transitHero() {
-    const interval = setInterval(() => {
-      if (spaceshipRef.current) {
-        clearInterval(interval);
-        const initialScale = spaceshipRef.current.scale;
+    const startReveal = () => {
         const tl = gsap.timeline();
+        gsap.set("#hero-section", { opacity: 1 });
+        gsap.set("#hero-title", { y: 100 });
+        gsap.set("#hero-subtitle", { x: -100, opacity: 0, y: 0 });
+        gsap.set("#hero-comingsoon", { y: 30, opacity: 0 });
+        gsap.set("#hero-countdown", { scale: 0.95, opacity: 0 });
 
         tl
-          .call(() => {
-            setPlayOne(true);
-          })
-          .to(spaceshipRef.current.position, {
-            x: 2.5,
-            y: 2.5,
-            z: 2.5,
-            duration: 1.5,
-            ease: "power2.out",
-            onComplete: () => {
-              setPlayTwo(true);
-            },
-          })
-          .to(spaceshipRef.current.rotation, {
-            y: -Math.PI / 8,
-            x: 0,
-            z: 0,
-            duration: 1.5,
-            ease: "power2.out",
-          }, "<")
-          .to(spaceshipRef.current.scale, {
-            x: initialScale.x * 0.5,
-            y: initialScale.y * 0.5,
-            z: initialScale.z * 0.5,
-            duration: 1.5,
-            ease: "power2.out",
-          }, "<")
-          .to(spaceshipRef.current.scale, {
-            x: initialScale.x * 2.5,
-            y: initialScale.y * 2.5,
-            z: initialScale.z * 2.5,
-            duration: 1.5,
-            ease: "power2.out",
-            onComplete: () => {
-              setPlayOne(false);
-              startReveal();
-            },
-          })
-          .to(spaceshipRef.current.position, {
-            x: 0,
-            y: 0,
-            z: 1,
-            duration: 1.5,
-            ease: "power2.out",
-          }, "<")
-          .to(spaceshipRef.current.rotation, {
-            y: Math.PI / 2,
-            x: -Math.PI / 2,
-            z: 0,
-            duration: 1.5,
-            ease: "power2.out",
-          }, "<")
-          .to(spaceshipRef.current.scale, {
-            x: initialScale.x,
-            y: initialScale.y,
-            z: initialScale.z,
-            duration: 1.5,
-            ease: "power2.out",
-          })
-          .to(spaceshipRef.current.position, {
-            x: 35,
-            y: 0,
-            z: 0,
-            duration: 1.5,
-            ease: "power2.out",
-          }, "<");
-      }
-    }, 100);
+            .to("#hero-title", {
+                duration: 2,
+                y: 0,
+                ease: "power2.out",
+                onComplete: () => {
+                    gsap.set("#model", { translateY: "100vh", position: "absolute", top: 0, left: 0, zIndex: "-10" });
+                },
+            })
+            .to("#hero-title", {
+                duration: 2,
+                y: 0,
+                ease: "power2.inOut",
+            })
+            .to("#hero-subtitle", {
+                x: 0,
+                opacity: 2,
+                duration: 1.2,
+                ease: "power3.out",
+            }, "-=0.5")
+            .set("#section-container", { display: "block" })
+            .to("#hero-comingsoon", {
+                y: 0,
+                opacity: 1,
+                duration: 2,
+                ease: "power2.out",
+                onComplete: () => {
+                    gsap.to("#section-container", {
+                        opacity: 1,
+                        duration: 0.5,
+                        ease: "power2.out",
+                    });
+                },
+            }, "-=0.8")
+            .to("#hero-countdown", {
+                scale: 1,
+                opacity: 1,
+                duration: 2,
+                ease: "back.out(1.7)",
+            }, "-=0.8");
+    };
 
-    return () => clearInterval(interval);
-  }
+    useEffect(() => {
+        startReveal()
+    },[])
 
-  useEffect(() => {
-    if (playOne) {
-      howlerOneRef.current.play();
-    } else {
-      howlerOneRef.current.stop();
-    }
+    return (
+        <>
+            <main   className="min-h-screen w-screen flex flex-col items-center justify-center overflow-x-hidden">
+                {/*<div id="model" className="fixed flex items-center justify-center top-0 left-0 z-[100] pointer-events-none">*/}
+                {/*  {isSpaceshipMounted && showSpaceship && <SpaceShipModel ref={spaceshipRef} />}*/}
+                {/*</div>*/}
+                {/*<PostLoading onComplete={transitHero} />*/}
+                <HeroSection />
+                <div id="section-container" className="h-full w-full hidden opacity-0">
+                    <NavBar />
 
-    if (playTwo) {
-      howlerTwoRef.current.play();
-    } else {
-      howlerTwoRef.current.stop();
-    }
-  }, [playOne, playTwo]);
+                    <section id="about">
+                        <AboutSection />
+                    </section>
 
-  return (
-    <SpaceshipContext.Provider value={spaceshipRef}>
-      <ReactHowler
-        src="/sounds/woof.mp3"
-        playing={false}
-        html5={true}
-        ref={howlerOneRef}
-        volume={1}
-      />
+                    <section id="ideas">
+                        <IdeaSection />
+                    </section>
 
-      <ReactHowler
-        src="/sounds/reveal.mp3"
-        playing={false}
-        html5={true}
-        ref={howlerTwoRef}
-        volume={1}
-        onEnd={() => {
-          setIsSpaceshipMounted(false);
-          gsap.to("#hero-title-glitch", {
-            opacity: 0,
-            duration: 0.5,
-            ease: "power2.out",
-          });
-        }}
-      />
-      <main ref={mainRef} className="min-h-screen w-screen flex flex-col items-center justify-center overflow-x-hidden">
-        <div id="model" className="fixed flex items-center justify-center top-0 left-0 z-[100] pointer-events-none">
-          {isSpaceshipMounted && showSpaceship && <SpaceShipModel ref={spaceshipRef} />}
-        </div>
-        <PostLoading onComplete={transitHero} />
-        <HeroSection />
-        <div id="section-container" className="h-full w-full hidden opacity-0">
-          <NavBar />
+                    <section id="proshow">
+                        <LazyProshow />
+                    </section>
 
-          <section id="about">
-            <AboutSection />
-          </section>
+                    <section id="autoshow">
+                        <LazyAutoshowSection />
+                    </section>
 
-          <section id="ideas">
-            <IdeaSection />
-          </section>
+                    <section id="workshop">
+                        <LazySwiperCoverflow />
+                    </section>
 
-          <section id="proshow">
-            <LazyProshow />
-          </section>
+                    <section id="faq">
+                        <FAQSection />
+                    </section>
 
-          <section id="autoshow">
-            <LazyAutoshowSection />
-          </section>
+                    <Footer />
 
-          <section id="workshop">
-            <LazySwiperCoverflow />
-          </section>
+                </div>
+            </main>
 
-          <section id="faq">
-            <FAQSection />
-          </section>
-
-          <Footer />
-
-        </div>
-      </main>
-    </SpaceshipContext.Provider>
-  );
+        </>
+    )
 }
+
+export default Home
