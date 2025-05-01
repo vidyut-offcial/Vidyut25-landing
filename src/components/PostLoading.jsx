830bc1b7e5db1567d40a8fd4fd88690d34f31b8b
devan <a.d.devnandan@gmail.com>
2025-05-02 00:42:25 +0530

    post loading enter done

--- src/components/PostLoading.jsx
+++ src/components/PostLoading.jsx
@@ -1,61 +1,49 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, {useEffect, useRef, useState} from "react";
 import { gsap } from "gsap";
 import ReactHowler from "react-howler";
 import TerminalStartup from "@/components/TerminalStarup";
 import Image from "next/image";
-import Logo from "../../public/images/logo_enhanced.png";
+import Logo from "../../public/images/logo.svg";
 
-const PostLoading = ({ setRevel }) => {
+function useIsMobile() {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const checkMobile = () => setIsMobile(window.innerWidth <= 768);
+        checkMobile();
+        window.addEventListener("resize", checkMobile);
+        return () => window.removeEventListener("resize", checkMobile);
+    }, []);
+    return isMobile;
+}
+
+const PostLoading = ({setRevel}) => {
     const logoRef = useRef(null);
     const terminalRef = useRef(null);
     const howlerOneRef = useRef();
     const howlerTwoRef = useRef();
     const howlerThreeRef = useRef();
-    const tapTextRef = useRef(null);
-    const [hasStarted, setHasStarted] = useState(false);
-    const [isMobile, setIsMobile] = useState(false);
-    const soundIds = useRef({
-        thunder: null,
-        rock: null,
-        metal: null
-    });
 
-    useEffect(() => {
-        const checkIfMobile = () => {
-            setIsMobile(window.innerWidth < 768);
-        };
-        checkIfMobile();
-        window.addEventListener("resize", checkIfMobile);
-        return () => window.removeEventListener("resize", checkIfMobile);
-    }, []);
+    const [hasInteracted, setHasInteracted] = useState(false);
 
-    const startFade = () => {
+    const startFade = ()=>{
+        console.log("start");
         setRevel(true);
-        gsap.to(logoRef.current, {
+        gsap.to(logoRef, {
             opacity: 0,
             duration: 1,
             ease: "power2.out",
         });
-    };
-
+    }
     const fadeInThenOut = (
         sound,
         id,
         element = null,
-        fadeDuration = isMobile ? 1000 : 2000,
-        holdDuration = isMobile ? 8000 : 15000
+        fadeDuration = 2000,
+        holdDuration = 10000
     ) => {
         return new Promise((resolve) => {
             sound.fade(0, 1, fadeDuration, id);
-
-            if (element) {
-                gsap.to(element, {
-                    opacity: 1,
-                    duration: fadeDuration / 1000,
-                    ease: "power2.out",
-                });
-            }
-
             setTimeout(() => {
                 sound.fade(1, 0, fadeDuration, id);
                 if (element) {
@@ -65,144 +53,110 @@ const PostLoading = ({ setRevel }) => {
                         ease: "power2.out",
                     });
                 }
-
-                setTimeout(() => {
-                    sound.stop(id);
-                    resolve();
-                }, fadeDuration);
+                setTimeout(resolve, fadeDuration);
             }, fadeDuration + holdDuration);
         });
     };
 
-    const fadeInSecond = (
-        element,
-        sound,
-        id,
-        fadeDuration = isMobile ? 1000 : 2000
-    ) => {
+    const fadeInSecond = (element, sound, id, fadeDuration = 2000) => {
         return Promise.all([
             element
                 ? gsap.to(element, {
-                      opacity: 1,
-                      duration: fadeDuration / 1000,
-                      ease: "power2.out",
-                  })
+                    opacity: 1,
+                    duration: fadeDuration / 1000,
+                    ease: "power2.out",
+                }).then()
                 : Promise.resolve(),
+
             new Promise((resolve) => {
                 sound.fade(0, 1, fadeDuration, id);
-                setTimeout(() => {
-                    resolve();
-                }, fadeDuration);
+                setTimeout(resolve, fadeDuration);
             }),
         ]);
     };
 
-    const playSound = (ref, soundName) => {
+    const playSound = (ref) => {
         return new Promise((resolve) => {
             const sound = ref.current?.howler;
             if (sound) {
-                if (sound.state() === "loaded") {
-                    const id = sound.play();
-                    soundIds.current[soundName] = id;
-                    resolve({ sound, id });
-                } else {
-                    sound.once("load", () => {
-                        const id = sound.play();
-                        soundIds.current[soundName] = id;
-                        resolve({ sound, id });
-                    });
-                }
-            } else {
-                resolve(null);
+                const id = sound.play();
+                resolve({ sound, id });
             }
         });
     };
 
-    // Clean up all sounds when component unmounts
-    useEffect(() => {
-        return () => {
-            if (howlerOneRef.current?.howler && soundIds.current.thunder) {
-                howlerOneRef.current.howler.stop(soundIds.current.thunder);
-            }
-            if (howlerTwoRef.current?.howler && soundIds.current.rock) {
-                howlerTwoRef.current.howler.stop(soundIds.current.rock);
-            }
-            if (howlerThreeRef.current?.howler && soundIds.current.metal) {
-                howlerThreeRef.current.howler.stop(soundIds.current.metal);
-            }
-        };
-    }, []);
-
-    useEffect(() => {
-        if (!hasStarted && !isMobile) {
-            startSequence();
-        }
-    }, [hasStarted, isMobile]);
-
-
     const startSequence = async () => {
-        if (hasStarted) return;
-        setHasStarted(true);
+        // Start thunder sound
+        const { sound: thunderSound, id: thunderId } = await playSound(howlerOneRef);
+        await fadeInThenOut(thunderSound, thunderId,terminalRef.current,2000, 10000); // 2s fade in, 10s hold, 2s fade out
 
-        try {
-            // Play thunder sound with terminal animation
-            const thunder = await playSound(howlerOneRef, 'thunder');
-            if (thunder) {
-                await fadeInThenOut(thunder.sound, thunder.id, terminalRef.current);
-            }
+        const { sound: secondSound, id: secondId } = await playSound(howlerTwoRef);
+        await fadeInSecond(logoRef.current, secondSound, secondId, 2000);
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+        const { sound: thridSound, id: thirdId } = await playSound(howlerThreeRef);
 
-            // Play rock sound with logo animation
-            const rock = await playSound(howlerTwoRef, 'rock');
-            if (rock) {
-                await fadeInSecond(logoRef.current, rock.sound, rock.id);
-                setTimeout(() => rock.sound.stop(rock.id), isMobile ? 2000 : 3000);
-            }
-
-            // Show tap text
-            await new Promise((res) => setTimeout(res, isMobile ? 1500 : 3000));
-            gsap.to(tapTextRef.current, {
-                opacity: 1,
-                duration: 1,
-                delay: 0.5,
-                ease: "power2.out",
-            });
-
-            // Play metal sound in background
-            const metal = await playSound(howlerThreeRef, 'metal');
-            if (metal) {
-                setTimeout(() => metal.sound.stop(metal.id), isMobile ? 5000 : 8000);
-            }
-        } catch (error) {
-            console.error("Error in audio sequence:", error);
-            gsap.to(logoRef.current, {
-                opacity: 1,
-                duration: 1,
-                ease: "power2.out",
-            });
-        }
     };
+
+    const isMobile = useIsMobile();
+
+  useEffect(() => {
+
+    if (!isMobile) {
+      startSequence();
+    }
+  }, [isMobile]);
+
+  // useEffect(() => {
+  //   const hasSeenLoading = sessionStorage.getItem("hasSeenLoadingScreen");
+  //
+  //   if (hasSeenLoading) {
+  //     // Skip animation
+  //     setRevel(true);
+  //   } else {
+  //     if (!isMobile) {
+  //       setHasInteracted(true);
+  //       startSequence().then(() => {
+  //         sessionStorage.setItem("hasSeenLoadingScreen", "true");
+  //       });
+  //     }
+  //   }
+  // }, [isMobile]);
+
+  const handleStart = () => {
+    setHasInteracted(true);
+    startSequence().then(() => {
+      sessionStorage.setItem("hasSeenLoadingScreen", "true");
+    });
+  };
 
     return (
         <div className="fixed inset-0 flex items-center justify-center bg-black overflow-hidden">
+            {!hasInteracted && isMobile && (
+                <div className="absolute inset-0 z-40 bg-black flex items-center justify-center">
+                    <button
+                        onClick={handleStart}
+                        className="text-white px-6 py-3 bg-gray-800 rounded-lg hover:bg-gray-700 transition"
+                    >
+                        Enter
+                    </button>
+                </div>
+            )}
+
             <ReactHowler
                 src="/sounds/thunder.mp3"
                 playing={false}
                 html5={true}
                 ref={howlerOneRef}
                 volume={1}
-                preload={true}
-                onLoad={() => console.log("Thunder sound loaded")}
-                onLoadError={(id, err) => console.error("Thunder load error", err)}
             />
+
+            {/* Second sound */}
             <ReactHowler
                 src="/sounds/rock.mp3"
                 playing={false}
                 html5={true}
                 ref={howlerTwoRef}
                 volume={1}
-                preload={true}
-                onLoad={() => console.log("Rock sound loaded")}
-                onLoadError={(id, err) => console.error("Rock load error", err)}
             />
             <ReactHowler
                 src="/sounds/metal.mp3"
@@ -210,49 +164,34 @@ const PostLoading = ({ setRevel }) => {
                 html5={true}
                 ref={howlerThreeRef}
                 volume={1}
-                preload={true}
-                onLoad={() => console.log("Metal sound loaded")}
-                onLoadError={(id, err) => console.error("Metal load error", err)}
             />
-            {/* Mobile-only "Enter" button */}
-            {!hasStarted && (
-                <button
-                    onClick={startSequence}
-                    className="md:hidden z-40 fixed bottom-10 px-6 py-3 bg-white text-black font-bold rounded-full shadow-lg animate-bounce transition-all duration-300"
-                >
-                    Enter
-                </button>
-            )}
-
-            <div ref={terminalRef} className="z-20 opacity-0">
+            <div ref={terminalRef} className="z-20">
                 <TerminalStartup />
             </div>
-
             <div
                 ref={logoRef}
-                className="z-30 absolute inset-0 flex flex-col items-center justify-center opacity-0"
+                className="z-30 absolute inset-0 flex flex-col items-center justify-center opacity-0 transition-opacity duration-1000"
             >
-                <button
-                    className="flex items-center justify-center"
-                    onClick={startFade}
-                    aria-label="Continue"
+                <button className="flex items-center justify-center"
+                        onClick={startFade}
                 >
                     <Image
-                        className="w-[80vw] max-w-[800px] h-auto object-contain cursor-pointer"
+                        className="w-[200px] sm:w-[240px] object-contain cursor-pointer"
                         src={Logo}
                         alt="Vidyut Logo"
+                        width={206}
+                        height={206}
                         priority
-                        quality={100}
-                        sizes="(max-width: 768px) 80vw, 800px"
                     />
                 </button>
                 <p
-                    ref={tapTextRef}
-                    className="text-white text-lg md:text-xl mt-4 opacity-0"
+                    ref={logoRef}
+                    className="mt-8.5 font-nexa"
                 >
-                    Tap to view
+                    Tap to continue
                 </p>
             </div>
+
         </div>
     );
 };
