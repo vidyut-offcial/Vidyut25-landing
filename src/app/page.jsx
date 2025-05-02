6c73dd89878c2f730422f878d8422a4e949341aa
devan <a.d.devnandan@gmail.com>
2025-05-03 01:15:14 +0530

    fixed types

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -108,7 +108,7 @@ const LazyIdeaSection = dynamic(() => import('@/components/IdeaSection'), {
     },[startRevel]);
 
 
-    // hai for docker test3
+    // hai for docker test4
     return (
         <>
             <ReactHowler
